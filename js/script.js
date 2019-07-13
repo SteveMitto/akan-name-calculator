466cc320bbc4dd9e06cd648bbf7627fff2a8df25
Steve <mittosteve@gmail.com>
2019-07-13 19:13:21 +0300

    This commit tries to make the date accurate by using .this

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var fname,sname,date,month,year,gender,names,cc,yearAct,centCalc;
+var fname,sname,date,month,year,gender,names,century,yearAct,dayOfWeek,actualDay;
 
 var days=["Sunday","Moday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
@@ -15,74 +15,59 @@ var dayFinder = function(){
   this.gender=document.forms["akanNames"]["gender"].value;
   this.names=fname.concat(" ",sname)
   console.log(names)
-  this.yearAct=year.slice(2,4);
-  this.centCalc=function(year1){
-    this.cc=year1;
-    if (year1 > 2000){
-      return 20;
-    }
-    else if(year1 <=2000 && year1 >=1901){
-      return 19;
-    }
-    else if(year1 <=1900 && year1 >= 1801){
-      return 18;
-    }else{
-      return false;
-    }
-  }
-  var century=centCalc(year);
-  alert(century)
+  this.yearAct=this.year.slice(2,4);
+  this.century=this.year.slice(0,2);
+  alert(this.century)
 
-  var dayOfWeek=(((century/4)-2*century-1) +((5*yearAct/4)) + ((26*(month + 1)/10))+date )%7;
+  this.dayOfWeek=( ( (this.century/4) -2*this.century-1) + ((5*this.yearAct/4) ) + ((26*(this.month+1)/10)) + this.date ) % 7;
+  this.actualDay= this.dayOfWeek.toFixed(0);
+  alert(this.actualDay);
+  if (this.gender === "male"){
 
-  var actualDay= dayOfWeek.toFixed(0);
-  alert(actualDay);
-  if (gender === "male"){
-
-    if(actualDay === 1){
-      alert("Hello " + names + " you were born on " + days[0] +" and your Akan Name is " + maleNames[0]);+ "."
+    if(this.actualDay === 1){
+      alert("Hello " + this.names + " you were born on " + this.days[0] +" and your Akan Name is " + this.maleNames[0]);+ "."
     }
-    else if (actualDay == 2) {
-      alert("Hello " + names + " you were born on " + days[1] +" and your Akan Name is " + maleNames[1]);+ "."
+    else if (this.actualDay == 2) {
+      alert("Hello " + this.names + " you were born on " + this.days[1] +" and your Akan Name is " + this.maleNames[1]);+ "."
     }
-    else if (actualDay == 3) {
-      alert("Hello " + names + " you were born on " + days[2] +" and your Akan Name is " + maleNames[2]);+ "."
+    else if (this.actualDay == 3) {
+      alert("Hello " + this.names + " you were born on " + this.ays[2] +" and your Akan Name is " + this.maleNames[2]);+ "."
     }
-    else if (actualDay == 4) {
-      alert("Hello " + names + " you were born on " + days[3] +" and your Akan Name is " + maleNames[3]);+ "."
+    else if (this.actualDay == 4) {
+      alert("Hello " + this.names + " you were born on " + this.days[3] +" and your Akan Name is " + this.aleNames[3]);+ "."
     }
-    else if (actualDay == 5) {
-      alert("Hello " + names + " you were born on " + days[4] +" and your Akan Name is " + maleNames[4]);+ "."
+    else if (this.actualDay == 5) {
+      alert("Hello " + this.names + " you were born on " + this.days[4] +" and your Akan Name is " + this.maleNames[4]);+ "."
     }
-    else if (actualDay == 6) {
-      alert("Hello " + names + " you were born on " + days[5] +" and your Akan Name is " + maleNames[5]);+ "."
+    else if (this.actualDay == 6) {
+      alert("Hello " + this.names + " you were born on " + this.days[5] +" and your Akan Name is " + this.maleNames[5]);+ "."
     }
-    else if (actualDay == 7 ) {
-      alert("Hello " + names + " you were born on " + days[6] +" and your Akan Name is " + maleNames[6]);+ "."
+    else if (this.actualDay == 7 ) {
+      alert("Hello " + this.names + " you were born on " + this.days[6] +" and your Akan Name is " + this.maleNames[6]);+ "."
     }else{
       alert("invalid")
     }
-  }else if(gender === "female"){
-    if(actualDay === 1){
-      alert("Hello " + names + " you were born on " + days[0] +" and your Akan Name is " + femaleNames[0]+ ".");
+  }else if(this.gender === "female"){
+    if(this.actualDay === 1){
+      alert("Hello " + this.names + " you were born on " + this.days[0] +" and your Akan Name is " + this.femaleNames[0]+ ".");
     }
-    else if (actualDay == 2) {
-      alert("Hello " + names + " you were born on " + days[1] +" and your Akan Name is " + femaleNames[1]+ ".");
+    else if (this.actualDay == 2) {
+      alert("Hello " + this.names + " you were born on " + this.days[1] +" and your Akan Name is " + this.femaleNames[1]+ ".");
     }
-    else if (actualDay == 3) {
-      alert("Hello " + names + " you were born on " + days[2] +" and your Akan Name is " + femaleNames[2]+ ".");
+    else if (this.actualDay == 3) {
+      alert("Hello " + this.names + " you were born on " + this.days[2] +" and your Akan Name is " + this.femaleNames[2]+ ".");
     }
-    else if (actualDay == 4) {
-      alert("Hello " + names + " you were born on " + days[3] +" and your Akan Name is " + femaleNames[3]+ ".");
+    else if (this.actualDay == 4) {
+      alert("Hello " + this.names + " you were born on " + this.days[3] +" and your Akan Name is " + this.femaleNames[3]+ ".");
     }
-    else if (actualDay == 5) {
-      alert("Hello " + names + " you were born on " + days[4] +" and your Akan Name is " + femaleNames[4]+ ".");
+    else if (this.actualDay == 5) {
+      alert("Hello " + this.names + " you were born on " + this.days[4] +" and your Akan Name is " + this.femaleNames[4]+ ".");
     }
-    else if (actualDay == 6) {
-      alert("Hello " + names + " you were born on " + days[5] +" and your Akan Name is " + femaleNames[5]+ ".");
+    else if (this.actualDay == 6) {
+      alert("Hello " + this.names + " you were born on " + this.days[5] +" and your Akan Name is " + this.femaleNames[5]+ ".");
     }
-    else if (actualDay == 7 ) {
-      alert("Hello " + names + " you were born on " + days[6] +" and your Akan Name is " + femaleNames[6]+ ".");
+    else if (this.actualDay == 7 ) {
+      alert("Hello " + this.names + " you were born on " + this.days[6] +" and your Akan Name is " + this.femaleNames[6]+ ".");
     }else{
       alert("invalid")
     }
